2c9df90b2b279ce850c7e1d75c47d21b725fd877
Rachael Njeri <njerirachael0@gmail.com>
2017-03-08 00:19:54 +0300

    fizzbuzz test

---
+++ fizzlab.py
@@ -0,0 +1,12 @@
+def fizz_buzz(j):
+  if j % 3==0:
+    return 'Fizz'
+  elif j % 5==0:
+    return 'Buzz'
+  elif j % 3==0 and j % 5==0:
+    return 'FizzBuz'
+  elif j % 3!=0 and j % 5!=0:
+  	print (j)
+  else:
+    return j
+
