80cb06f04c12e8ae25b09666ccb9beb8a3c86cf6
Rachael Njeri <njerirachael0@gmail.com>
2017-03-08 06:49:01 +0300

    return message

--- fizzlab.py
+++ fizzlab.py
@@ -1,12 +1,12 @@
 def fizz_buzz(j):
   if j % 3==0:
-    return 'Fizz'
-  elif j % 5==0:
-    return 'Buzz'
+    return 'Fizz' "msg('Fizz for number divisible by 3')"
+  elif j % 5==0:  
+    return 'Buzz' "msg('Buzz for number divisible by 5')"
   elif j % 3==0 and j % 5==0:
-    return 'FizzBuz'
+    return 'FizzBuz' "msg('FizzBuzz for number divisible by 3 and 5')"
   elif j % 3!=0 and j % 5!=0:
-  	print (j)
+  	print (j)     "msg('print the number if indivisible by 3 neither 5')"
   else:
     return j
 
