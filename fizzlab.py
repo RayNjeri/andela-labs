ae20e89d30ac6ecfe7719207a8ff0944fa4217cd
Rachael Njeri <njerirachael0@gmail.com>
2017-03-08 07:10:23 +0300

    error ommsion and correction

--- fizzlab.py
+++ fizzlab.py
@@ -1,12 +1,11 @@
 def fizz_buzz(j):
+  if j % 3==0 and j % 5==0:
+    return 'FizzBuzz'
   if j % 3==0:
-    return 'Fizz' "msg('Fizz for number divisible by 3')"
-  elif j % 5==0:  
-    return 'Buzz' "msg('Buzz for number divisible by 5')"
-  elif j % 3==0 and j % 5==0:
-    return 'FizzBuz' "msg('FizzBuzz for number divisible by 3 and 5')"
-  elif j % 3!=0 and j % 5!=0:
-  	print (j)     "msg('print the number if indivisible by 3 neither 5')"
+    return 'Fizz'
+  elif j % 5==0:
+    return 'Buzz'
   else:
     return j
+  
 
